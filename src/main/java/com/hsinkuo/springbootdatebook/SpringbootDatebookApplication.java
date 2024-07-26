64e3d4e4fdcb5df8d6e82b11740c9bd1af172c6b
Hsin <ketty31kuo@gmail.com>
2024-07-26 13:53:20 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/hsinkuo/springbootdatebook/SpringbootDatebookApplication.java
@@ -0,0 +1,13 @@
+package com.hsinkuo.springbootdatebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsinkuo/springbootdatebook/SpringbootDatebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsinkuo.springbootdatebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
